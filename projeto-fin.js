0cbd1a6a7eb090d503dc9d2119aaeae8832afbe9
erickwiatkowski <erickwiatkowski4@gmail.com>
2022-09-27 22:48:23 -0300

    0.004

--- projeto-fin.js
+++ projeto-fin.js
@@ -1,16 +1,18 @@
 let nome = prompt("insira seu nome!!!");
 let idade = parseInt(prompt("insira sua idade"));
 
-    let investimento = investimento;
-    let valorDeAcao = valorDeAcao;
-    let dividendosPagos = dividendosPagos;
+    let investimento = 0;
+    let valorDeAcao = 0;
+    let dividendosPagos = 0;
        function comfirmar(){
         investimento = document.getElementsByName("investimento").value;
         valorDeAcao = document.getElementsByName("valorDeAcao").value;
-        dividendosPagos = document.getElementsByName("dividendosPagos").value;
+        dividendosPagos = document.getElementsById("dividendosPagos").value;
 
     }
+    document.querySelector('#comfirmar')
+.onclick = comfirmar;
+
 document.write(investimento);
-document.write(investimento());
 
 console.log(investimento);
